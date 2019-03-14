09519139583912de473cf82973c3b9f5f6d0a065
maria.fatu <fatumaria12@gmail.com>
2019-03-14 22:39:30 +0200

    removed log from console and added values for light agent

--- src/rooms/LivingRoomAgent.java
+++ src/rooms/LivingRoomAgent.java
@@ -9,6 +9,7 @@ import es.upv.dsic.gti_ia.core.ACLMessage;
 import es.upv.dsic.gti_ia.core.AgentID;
 import es.upv.dsic.gti_ia.core.SingleAgent;
 import static java.lang.Thread.sleep;
+import java.util.Random;
 
 /**
  *
@@ -21,6 +22,7 @@ public class LivingRoomAgent extends SingleAgent {
     private int humidity;
     
     boolean gotMsg = false;
+    int[] valuesArray={60,70,56,33,67};
     
     public LivingRoomAgent(AgentID aid,int temp,int light,int humidity) throws Exception {
         super(aid);
@@ -32,16 +34,21 @@ public class LivingRoomAgent extends SingleAgent {
     public void onMessage(ACLMessage msg){
         System.out.println("Hi! I'm agent "+this.getName()+" and I've received the message: "+msg.getContent());
     }
+    
+    public static int getRandomFromArray(int[] array){
+        int rnd =new Random().nextInt(array.length);
+        return array[rnd];
+    }
      public void execute(){
         System.out.println("Hi! I'm agent "+this.getName()+" and I start my execution");
         ACLMessage msg = new ACLMessage();
         ACLMessage send=new ACLMessage();
          try {
                 send.setReceiver(new AgentID("LightAgent"));
-                send.setContent("Fiat Lux at ");
                 send.setSender(this.getAid());
                 while(true){
                     sleep(3000);
+                    send.setContent("Fiat Lux by "+getRandomFromArray(valuesArray)+" %");
                     this.send(send);
                 }
                
